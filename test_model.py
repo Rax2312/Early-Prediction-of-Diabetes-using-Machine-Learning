634a1c9dc18f75d7b20a53868da49078871f7364
Rax2312 <142116760+Rax2312@users.noreply.github.com>
2025-05-08 16:40:50 +0530

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -1,14 +1,11 @@
 import tensorflow as tf
 import numpy as np
 
-# 1. Load model
 model = tf.saved_model.load('ml_model/fixed_model')
 infer = model.signatures['serving_default']
 
-# 2. Create test input (matches your preprocessed shape)
 dummy_input = np.random.rand(1, 1, 16).astype(np.float32)  # Must be float32!
 
-# 3. Predict
 output = infer(tf.constant(dummy_input))
 print("Prediction shape:", output['output_0'].shape)  # Should be (1, 3)
 print("Sample prediction:", output['output_0'].numpy())
